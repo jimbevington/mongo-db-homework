4077657b7b1c7cfeb2c32315c1fd23e8a5e688e1
Jim Bevington <jim.bevington@hotmail.com>
2018-04-17 16:39:42 +0100

    connected to MongoClient, moved server.listen inside connect callback function

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ server.use(parser.urlencoded({extended: true}));
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
-server.listen(3000, function(){
-  console.log("Listening on port 3000");
-});
+MongoClient.connect("mongodb://localhost:27017", function(err, client){
+  if(err){
+    console.log(err);
+    return;
+  }
+
+
+
+
+  server.listen(3000, function(){
+    console.log("Listening on port 3000");
+  });
+})
