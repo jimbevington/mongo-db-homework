9f62dc0c8053a340341293e896c668ff436b3a64
Jim Bevington <jim.bevington@hotmail.com>
2018-04-18 09:15:51 +0100

    Added getSynthsRequestComplete

--- client/src/app.js
+++ client/src/app.js
@@ -4,6 +4,10 @@ const Request = require('./services/request.js');
 const synthView = new SynthView();
 const request = new Request('http://localhost:3000/api/synths');
 
+const getSynthsRequestComplete = function(allSynths){
+  allSynths.forEach(synth => SynthView.addSynth(synth));
+}
+
 const appStart = function(){
   request.get(getSynthsRequestComplete);
 }
