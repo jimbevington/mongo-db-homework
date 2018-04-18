7470c048fc5d16b759021ed6839ff787c620316a
Jim Bevington <jim.bevington@hotmail.com>
2018-04-18 09:24:36 +0100

    added createButtonClicked

--- client/src/app.js
+++ client/src/app.js
@@ -8,10 +8,25 @@ const getSynthsRequestComplete = function(allSynths){
   allSynths.forEach(synth => synthView.addSynth(synth));
 }
 
+const createButtonClicked = function(event){
+  event.preventDefault();
+
+  const nameInputValue = document.getElementById('name').value;
+  const manuInputValue = document.getElementById('manufacturer').value;
+
+  const synthToSave = {
+    name: nameInputValue,
+    manufacturer: manuInputValue
+  };
+
+  request.post(createRequestComplete, synthToSave);
+}
+
 const appStart = function(){
   request.get(getSynthsRequestComplete);
 
   const createSynthButton = document.getElementById('submit-synth');
+  createSynthButton.addEventListener('click', createButtonClicked);
 }
 
 document.addEventListener('DOMContentLoaded', appStart);
